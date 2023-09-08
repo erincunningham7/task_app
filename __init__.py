a36b647d97ccc089d1963e1ef7cd0602cc8488ec
erincunningham7 <>
2023-09-08 10:41:39 +0000

    create instances of imported classes in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,17 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+# In order to use our hidden environment variables, need to import the 'env' package.
 if os.path.exists("env.py"):
-    import env
+    import env  # noqa
+
+# create an instance of the imported flask class
+app = Flask(__name__)
+# specify two app configuration variables
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+# create an instance of the imported sqlalchemy class
+db = SQLAlchemy(app)
+
+from taskmanager import routes  # noqa
